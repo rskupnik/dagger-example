c2c9399bae83a0b8603b728df8e03c52f24990ca
Myzreal <r.skupnik@gmail.com>
2017-08-31 20:30:59 +0200

    Rename SomeModule to ProvidersModule

--- src/main/java/com/github/rskupnik/ComputerInjector.java
+++ src/main/java/com/github/rskupnik/ComputerInjector.java
@@ -1,10 +1,10 @@
 package com.github.rskupnik;
 
-import com.github.rskupnik.components.SomeModule;
+import com.github.rskupnik.components.ProvidersModule;
 import com.github.rskupnik.components.implementations.Computer;
 import dagger.Component;
 
-@Component(modules = SomeModule.class)
+@Component(modules = ProvidersModule.class)
 public interface ComputerInjector {
     Computer computer();
 }
--- src/main/java/com/github/rskupnik/components/ProvidersModule.java
+++ src/main/java/com/github/rskupnik/components/ProvidersModule.java
@@ -6,7 +6,7 @@ import dagger.Module;
 import dagger.Provides;
 
 @Module
-public class SomeModule {
+public class ProvidersModule {
 
     @Provides
     Printer printer() {
